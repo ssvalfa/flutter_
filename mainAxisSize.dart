672d597179cddf7f0bd3cfafacb0f9ca7d3b439c
ssvalfa <160457690+ssvalfa@users.noreply.github.com>
2024-11-13 20:37:30 +0600

    Add files via upload

---
+++ mainAxisSize.dart
@@ -0,0 +1,34 @@
+// ignore: duplicate_ignore
+// ignore: file_names
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Container(
+              color: Colors.black,
+              height: 200,
+            ),
+            Container(
+              color: Colors.blue,
+              height: 200,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
